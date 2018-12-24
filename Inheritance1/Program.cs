35632621142de2ff6594cb35d292190011a03802
Vitor Perez <vitor.g.perez@hotmail.com>
2018-12-24 08:41:50 -0200

    Entered commands and instantiated classes

--- Inheritance1/Program.cs
+++ Inheritance1/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using Inheritance1.Entities;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,38 @@ namespace Inheritance1
     {
         static void Main(string[] args)
         {
+            List<TaxPayer> list = new List<TaxPayer>();
+
+            Console.Write("Enter the number of tax payers: ");
+            int number = int.Parse(Console.ReadLine());
+            for(int x = 1; x <= number; x++)
+            {
+                Console.WriteLine($"Tax payer #{x} data:");
+                Console.Write("Individual or Company (i/c): ");
+                char question = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Anual income: ");
+                double anualIncome = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if(question == 'i')
+                {
+                    Console.Write("Health Expenditures : ");
+                    double healthExpenditures = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new Individual(name, anualIncome, healthExpenditures));
+                }
+                else
+                {
+                    Console.Write("Number of employees: ");
+                    int numberOfEmployees = int.Parse(Console.ReadLine());
+                    list.Add(new Company(name, anualIncome, numberOfEmployees));
+                }
+            }
+
+            Console.WriteLine("\nTAXES PAID: ");
+            foreach(TaxPayer txp in list)
+            {
+                Console.WriteLine(txp.TaxPaid());
+            }
         }
     }
 }
